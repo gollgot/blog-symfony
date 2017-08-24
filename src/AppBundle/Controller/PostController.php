21f9a56c3fec08b0bb6d46b6b98ee95a462f03d2
Dessaules Loïc <loic.dessaules@gmail.com>
2017-08-24 15:29:47 +0200

    Updated roles

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Comment;
 use AppBundle\Entity\Post;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
