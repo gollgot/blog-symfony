02976e869b7466b2a21747eae4e6b9730667923e
Dessaules Loïc <loic.dessaules@gmail.com>
2017-08-30 11:52:19 +0200

    Updated some config for Rest bundle, and added getPostAction

--- src/ApiBundle/Controller/PostController.php
+++ src/ApiBundle/Controller/PostController.php
@@ -2,16 +2,18 @@
 
 namespace ApiBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use FOS\RestBundle\Controller\Annotations\Get;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
 	/**
-	 * @Route("/posts", name="api_posts_list")
-	 * @Method({"GET"})
+	 * @Get("/posts/")
 	 */
 	public function getPostsAction()
 	{
@@ -43,4 +45,41 @@ class PostController extends Controller
 		return new JsonResponse($formatted);
 
 	}
+
+	/**
+	 * @Get("/posts/{id}")
+	 */
+	public function getPostAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$post = $em->getRepository('AppBundle:Post')->find($request->get('id'));
+
+		// The Post doesn't exists => return error message with the status code 404
+		if (empty($post)) {
+			return new JsonResponse(['message' => 'Post not found'], Response::HTTP_NOT_FOUND);
+		}
+
+		// Author
+		if(!empty($post->getAuthor())){
+			$author = $post->getAuthor();
+			$authorArray[] = [
+				'id' => $author->getId(),
+				'name' => $author->getUsername()
+			];
+		}else{
+			$authorArray = null;
+		}
+
+		// All formated datas
+		$formatted[] = [
+			'id' => $post->getId(),
+			'title' => $post->getTitle(),
+			'created_at' => $post->getCreatedAt(),
+			'author' => $authorArray,
+		];
+
+		// Default JsonResponse used a 200 status code
+		return new JsonResponse($formatted);
+	}
+
 }
