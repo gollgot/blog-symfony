72abef2a64bb32a675f1977bc5686ad60a6c9b1e
Dessaules Loïc <loic.dessaules@gmail.com>
2017-08-31 14:35:40 +0200

    Updated PostController api v1, return xml or json for posts and post/id
    I updated the api/v1/PostController to respect all the REST convention. For the getPostsAction and getPostAction, both return JSON or XML response.
    
    Default is JSON  but if the user send an HTTP header "Accept" with "application/xml", the API return an XML format.
    
    And I also add a name to the 2 routes

--- src/ApiBundle/Controller/v1/PostController.php
+++ src/ApiBundle/Controller/v1/PostController.php
@@ -12,42 +12,93 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends Controller
 {
 	/**
-	 * @Route("/posts/")
+	 * @Route("/posts/", name="api_v1_get_posts")
 	 * @Method("GET")
 	 */
-	public function getPostsAction()
+	public function getPostsAction(Request $request)
 	{
 		$em = $this->getDoctrine()->getManager();
 		$posts = $em->getRepository('AppBundle:Post')->findBy(array(), array('createdAt' => 'DESC'));
 
-		$formatted = [];
-		foreach ($posts as $post) {
-			// Author
-			if(!empty($post->getAuthor())){
-				$author = $post->getAuthor();
-				$authorArray[] = [
-					'id' => $author->getId(),
-					'name' => $author->getUsername()
-				];
-			}else{
-				$authorArray = null;
-			}
+		// Get the "Accept" param in the http headers request from the client
+		switch($request->headers->get('accept')){
+			// user want a XML response
+			case 'application/xml':
+				// Header
+				$xml = '<?xml version="1.0" encoding="UTF-8"?>';
+				// The Post doesn't exists => create response with error message and the status code 404
+				if (empty($posts)) {
+					$xml .= '<error><code>404</code><message>Posts not found</message></error>';
+					$response = new Response($xml, Response::HTTP_NOT_FOUND);
+				}
+				// The posts exists
+				else{
+					$xml .= '<posts>';
+					foreach ($posts as $post) {
+						// Author of the post
+						if (!empty($post->getAuthor())) {
+							$author = $post->getAuthor();
+							$xmlAuthor = '<author><id>'.$author->getId().'</id><username>'.$author->getUsername().'</username></author>';
+						}else{
+							$xmlAuthor = '<author></author>';
+						}
+						$xml .= '<post><id>'.$post->getId().'</id><title>'.$post->getTitle().'</title><created_at>'.$post->getCreatedAt()->format('Y-m-d H:i').'</created_at>'.$xmlAuthor.'</post>';
+					}
+					$xml .= '</posts>';
+					$response = new Response($xml, Response::HTTP_OK);
+				}
 
-			// All formated datas
-			$formatted[] = [
-				'id' => $post->getId(),
-				'title' => $post->getTitle(),
-				'created_at' => $post->getCreatedAt(),
-				'author' => $authorArray,
-			];
+				// Set the header "Content-Type" to the http response
+				$response->headers->set('Content-Type', 'application/xml');
+				return $response;
+				break;
+			// Default response in JSON
+			default:
+				// The Post doesn't exists => create response with error message and the status code 404
+				if (empty($posts)) {
+					$json = [
+						'error' => [
+							'code'    => '404',
+							'message' => 'Posts not found',
+						],
+					];
+
+					$response = new JsonResponse($json, Response::HTTP_NOT_FOUND);
+				}
+				// The post exists
+				else {
+					foreach ($posts as $post) {
+						// Author of the post
+						if (!empty($post->getAuthor())) {
+							$author = $post->getAuthor();
+							$authorArray = [
+								'id'   => $author->getId(),
+								'name' => $author->getUsername()
+							];
+						} else {
+							$authorArray = null;
+						}
+
+						// All formated datas
+						$json[] = [
+							'id'         => $post->getId(),
+							'title'      => $post->getTitle(),
+							'created_at' => $post->getCreatedAt()->format('Y-m-d H:i'),
+							'author'     => $authorArray,
+						];
+
+						$response = new JsonResponse($json, Response::HTTP_OK);
+					}
+				}
+				// Set the header "Content-Type" to the http response
+				$response->headers->set('Content-Type', 'application/json');
+				return $response;
+				break;
 		}
-
-		return new JsonResponse($formatted);
-
 	}
 
 	/**
-	 * @Route("/posts/{id}")
+	 * @Route("/posts/{id}", name="api_v1_get_post")
 	 * @Method("GET")
 	 */
 	public function getPostAction(Request $request)
@@ -55,32 +106,75 @@ class PostController extends Controller
 		$em = $this->getDoctrine()->getManager();
 		$post = $em->getRepository('AppBundle:Post')->find($request->get('id'));
 
-		// The Post doesn't exists => return error message with the status code 404
-		if (empty($post)) {
-			return new JsonResponse(['message' => 'Post not found'], Response::HTTP_NOT_FOUND);
+		// Get the "Accept" param in the http headers request from the client
+		switch($request->headers->get('accept')){
+			// user want a XML response
+			case 'application/xml':
+				// Header
+				$xml = '<?xml version="1.0" encoding="UTF-8"?>';
+				// The Post doesn't exists => create response with error message and the status code 404
+				if (empty($post)) {
+					$xml .= '<error><code>404</code><message>Post not found</message></error>';
+					$response = new Response($xml, Response::HTTP_NOT_FOUND);
+				}
+				// The post exists
+				else{
+					// Author of the post
+					if (!empty($post->getAuthor())) {
+						$author = $post->getAuthor();
+						$xmlAuthor = '<author><id>'.$author->getId().'</id><username>'.$author->getUsername().'</username></author>';
+					}else{
+						$xmlAuthor = '<author></author>';
+					}
+					$xml .= '<post><id>'.$post->getId().'</id><title>'.$post->getTitle().'</title><created_at>'.$post->getCreatedAt()->format('Y-m-d H:i').'</created_at>'.$xmlAuthor.'</post>';
+					$response = new Response($xml, Response::HTTP_OK);
+				}
+
+				// Set the header "Content-Type" to the http response
+				$response->headers->set('Content-Type', 'application/xml');
+				return $response;
+				break;
+			// Default response in JSON
+			default:
+				// The Post doesn't exists => create response with error message and the status code 404
+				if (empty($post)) {
+					$json = [
+						'error' => [
+							'code'    => '404',
+							'message' => 'Post not found',
+						],
+					];
+
+					$response = new JsonResponse($json, Response::HTTP_NOT_FOUND);
+				}
+				// The post exists
+				else {
+					// Author of the post
+					if (!empty($post->getAuthor())) {
+						$author = $post->getAuthor();
+						$authorArray = [
+							'id'   => $author->getId(),
+							'name' => $author->getUsername()
+						];
+					} else {
+						$authorArray = null;
+					}
+
+					// All formated datas
+					$json = [
+						'id'         => $post->getId(),
+						'title'      => $post->getTitle(),
+						'created_at' => $post->getCreatedAt()->format('Y-m-d H:i'),
+						'author'     => $authorArray,
+					];
+
+					$response = new JsonResponse($json, Response::HTTP_OK);
+				}
+				// Set the header "Content-Type" to the http response
+				$response->headers->set('Content-Type', 'application/json');
+				return $response;
+				break;
 		}
-
-		// Author
-		if(!empty($post->getAuthor())){
-			$author = $post->getAuthor();
-			$authorArray[] = [
-				'id' => $author->getId(),
-				'name' => $author->getUsername()
-			];
-		}else{
-			$authorArray = null;
-		}
-
-		// All formated datas
-		$formatted[] = [
-			'id' => $post->getId(),
-			'title' => $post->getTitle(),
-			'created_at' => $post->getCreatedAt(),
-			'author' => $authorArray,
-		];
-
-		// Default JsonResponse used a 200 status code
-		return new JsonResponse($formatted);
 	}
 
 }
